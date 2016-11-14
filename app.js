7a9b358c7772c771e24c23d370cb61c36a453fff
manuelWeb <devries.webdesigner@gmail.com>
2016-11-14 17:58:59 +0100

    fct getFile add filtre ignore non img file

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ function getFiles (dir, files_){
   files_ = files_ || [];
   var files = fs.readdirSync(dir);
   for (var i in files){
-    // var name = dir + '/' + files[i];
-    // fs.statSync(name).isDirectory() ? getFiles(name, files_) : files_.push(name);
     var name = files[i];
-    files_.push(name)
+    !/^s[a0-b9]+\.(jpg|gif|png)$/gi.test(name) ? console.log('ignore: ' +name) : files_.push(name)
   }
   return files_;
 }
-console.log(getFiles('images'))
-
 var aryimg = getFiles('images')
+console.log('aryimg[0]' + aryimg[0])
+
+
 // console.log('typeof aryimg: '+typeof aryimg)
 var table = 'table', tr = 'tr', td = 'td', img ='img src="" alt=""';
 
