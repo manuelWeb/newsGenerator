db9eaeadb37d95fdb4ca9ae3b6303e1d756e2463
manuelWeb <devries.webdesigner@gmail.com>
2016-11-15 11:03:15 +0100

    nettoyage code; todo liste en slice du array img

--- app.js
+++ app.js
@@ -13,11 +13,10 @@ var aryimg = getFiles('images')
 console.log(aryimg)
 
 function slice(value){
-  // var aryl = value.length;
-  // console.log(aryl);
   return /^s1.+\.(jpg|gif|png)$/.test(value);
 }
-// console.log(aryimg.filter(slice))
+console.log(aryimg.filter(slice))
+
 function sortSlice(imgarray, slice){
   var re = new RegExp("s" + slice + ".+\.(jpg|gif|png)","gi"),
       result = [];
@@ -26,13 +25,9 @@ function sortSlice(imgarray, slice){
   }
   return result;
 }
-// console.log(sortSlice(aryimg,'2'))
-sortSlice(aryimg, '1');
-console.log(sortSlice(aryimg,'1'));
+// console.log(sortSlice(aryimg,'1'));
 
-// console.log('typeof aryimg: '+typeof aryimg)
-var table = 'table', tr = 'tr', td = 'td', img ='img src="" alt=""';
-
+// écrire dans un fichier
 function writeTable (imglist){
   fs.writeFile("var.slim", imglist, function(err){
     if(err) return console.log(err);
@@ -40,4 +35,3 @@ function writeTable (imglist){
   })
 }
 
-// writeTable(aryimg)
