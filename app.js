68798a9db58e174f9e824aa6e63fae62e776fffd
manuelWeb <devries.webdesigner@gmail.com>
2016-11-16 13:10:52 +0100

    fct cptSlice niv1 et affiche img par slice OK

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var fs = require('fs');
-
+var fs     = require('fs'),
+    aryimg = getFiles('images'),
+    slNiv1 = cptSlice(aryimg);
+// retourne array des fichiers présents dans images folder
 function getFiles (dir, files_){
   files_ = files_ || [];
   var files = fs.readdirSync(dir);
@@ -9,25 +11,25 @@ function getFiles (dir, files_){
   }
   return files_;
 }
-var aryimg = getFiles('images')
-console.log(aryimg)
-
-var s = '1';
+// fct pour filtrer slices s=1 sort [ 's1c1.jpg', 's1c2.jpg', 's1c3.jpg' ]
 function slice (ary){
-  var dynReg = new RegExp('^s'+s+'.+\.(jpg|gif|png)$','gi')
+  var dynReg = new RegExp('^s'+s+'.+\.(jpg|gif|png)$','gi');
   return dynReg.test(ary);
 }
-// console.log(aryimg.filter(slice))
-
+// cptSlice retourne le nombre totale de slices sncn=niv1
 function cptSlice (ary) {
   var tabs = [], result = [];
-  for (var i in ary) {
-    tabs.push(ary[i].slice(1,2))
-  }
+  for (var i in ary) { tabs.push(ary[i].slice(1,2)); }
   tabs.filter(function(item, pos) {
     tabs.indexOf(item) == pos ? result.push(item) : item++;
   })
   return result;
 }
-var slNiv1 = cptSlice(aryimg);
-console.log(slNiv1)
+// affiche s = numero slice
+for (var s in slNiv1) {
+  var s = ++s; // initialise s à 1 non à 0
+  console.log(s); // s = slice unique
+  // console.log(aryimg.filter(slice)); // = images par slice
+  var imgOnSce = function () { return aryimg.filter(slice); }
+  console.log(imgOnSce())
+}
