be30636eb470506ac2cf17f07e80e60329c203ca
manuelWeb <devries.webdesigner@gmail.com>
2016-11-19 12:07:37 +0100

    génération niveau1 + link: ok; todo niveau > 1 generation tab>tr>td>(img|link>img)

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ function getFiles (dir, files_){
 }
 // fct pour filtrer slices s=1 sort [ 's1c1.jpg', 's1c2.jpg', 's1c3.jpg' ]
 function slice (ary){
-  var dynReg = new RegExp('^s'+s+'.+\.(jpg|gif|png)$','gi');
+  // var dynReg = new RegExp('^s'+s+'.+\.(jpg|gif|png)$','gi');
+  var dynReg = new RegExp('^s'+s+'..(_l|)\.(jpg|gif|png)$','gi');
   return dynReg.test(ary);
 }
 // cptSlice retourne le nombre totale de slices sncn=niv1
@@ -21,7 +22,7 @@ function cptSlice (ary) {
   var tabs = [], result = [];
   for (var i in ary) { tabs.push(ary[i].slice(1,2)); }
   tabs.filter(function(item, pos) {
-    tabs.indexOf(item) == pos ? result.push(item) : item++;
+    if ( tabs.indexOf(item) == pos ) result.push(item);
   })
   return result;
 }
@@ -30,10 +31,19 @@ for (var s in slNiv1) {
   var s = ++s; // initialise s à 1 non à 0
   console.log('tr')// console.log(s); // s = slice unique
   // console.log(aryimg.filter(slice)); // = images par slice
-  var imgOnSce = function () { return aryimg.filter(slice); }
-  // console.log('tab'+s+'  ' +imgOnSce()+' typeof '+typeof imgOnSce())
-  // console.log('tab'+s+'  ' +imgOnSce())
+  var tabs1 = [];
+  var imgOnSce = function () {
+    return aryimg.filter(slice);
+  }
+  tabs1.push(aryimg.filter(slice))
+  // console.log(tabs1[0]);
   for (i in imgOnSce()){
-    console.log('  td' + '\n    ' + 'img src=' +imgOnSce()[i])
+    // console.log('  td' + '\n    ' +imgOnSce()[i].match(/_l/)!='' ? 'a href'+'\n'+'img src=' +imgOnSce()[i] : 'img src=' +imgOnSce()[i])
+    var item = imgOnSce()[i];
+    if (item.match(/_l/) == null){
+      console.log('  td' + '\n    ' + item)
+    }else{
+      console.log('  td' + '\n    ' + 'a href=($'+item.slice(0,4)+')' +'\n      ' + item)
+    }
   }
 }
