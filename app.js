9313eb94173f199eb8c9a8dbf62f3f4f8d7bd075
manuelWeb <devries.webdesigner@gmail.com>
2016-11-22 19:19:16 +0100

    travailler sur les tables imbriquées br=br2

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var fs = require('fs');
-
+var fs     = require('fs'),
+    aryimg = getFiles('images'),
+    slNiv1 = cptSlice(aryimg);
+console.log(cptSlice(aryimg));
+// retourne array des fichiers présents dans images folder
 function getFiles (dir, files_){
   files_ = files_ || [];
   var files = fs.readdirSync(dir);
@@ -9,24 +12,21 @@ function getFiles (dir, files_){
   }
   return files_;
 }
-var aryimg = getFiles('images')
-console.log(aryimg)
-
-function sortSlice(imgarray, slice){
-  var re = new RegExp("s" + slice + ".+\.(jpg|gif|png)","gi"),
-      result = [];
-  for (var i in imgarray){
-    result.push(imgarray[i].match(re))
-  }
+// fct pour filtrer slices s=1 sort [ 's1c1.jpg', 's1c2.jpg', 's1c3.jpg' ]
+function slice (ary){
+  var dynReg = new RegExp('^s'+s+'.+\.(jpg|gif|png)$','gi');
+  return dynReg.test(ary);
+}
+// cptSlice retourne le nombre totale de slices sncn=niv1
+function cptSlice (ary) {
+  var tabs = [], result = [];
+  for (var i in ary) { tabs.push(ary[i].slice(1,2)); }
+  tabs.filter(function(item, pos) {
+    if ( tabs.indexOf(item) == pos ) result.push(item);
+  })
   return result;
 }
-console.log(sortSlice(aryimg,'1'));
-
-// écrire dans un fichier
-function writeTable (imglist){
-  fs.writeFile("var.slim", imglist, function(err){
-    if(err) return console.log(err);
-    console.log('the var.slim was write')
-  })
+for (s in slNiv1) {
+  ++s;
+  console.log(aryimg.filter(slice))
 }
-
